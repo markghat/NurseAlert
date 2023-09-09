7c34e198e1b710dc7fef03ae00ed9e9e79484305
tomasesber <tomas.esber@duke.edu>
2023-09-09 00:21:22 -0400

    Creating xcode project for application user interface.

---
+++ NurseAlert/NurseAlert/NurseAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  NurseAlertApp.swift
+//  NurseAlert
+//
+//  Created by Tomas Esber on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct NurseAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

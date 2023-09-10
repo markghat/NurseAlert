02042089864667aa38df17f9a3b463847abbcf50
Mark Ghattas <mark.ghattas@duke.edu>
2023-09-10 01:43:30 -0400

    buttons

--- NurseAlert/NurseAlert/HospitalView.swift
+++ NurseAlert/NurseAlert/HospitalView.swift
@@ -28,42 +28,46 @@ struct HospitalView: View{
     
     
     var body: some View {
-        List (){
-            ForEach(formatHospitals().keys.sorted(by: <), id: \.self){ key in
-                Section(header: Text(key).foregroundColor(Color.black)) {
-                    VStack(alignment: .leading, spacing: 0){
-                        ForEach(formatHospitals()[key]!, id: \.self) { hospital in
-                            HStack{
-                                Image(hospital.image_url)
-                                    .resizable()
-                                    .frame(width: 30, height: 20)
-                                Text(hospital.hospital_name)
-                                    .foregroundColor(Color.black)
-                                Spacer()
-                                Image(systemName: "arrow.right")
-                                    .foregroundColor(Color.black)
-                            }//: HSTACK
-                            .padding(20)
-                            Rectangle()
-                                .frame(height: 2) // Adjust the height as needed
-                                .foregroundColor(Color.black)
-                        }//: LOOP
-                    }//: VSTACK
-                    .overlay(
-                        RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.black, lineWidth: 2)
-                    )
-                    .foregroundColor(Color(uiColor: .tertiaryLabel))
-                }//: SECTION
-                .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
-            }//: LOOP
-        }//: LIST
-        .listStyle(InsetListStyle())
+            NavigationView { // Wrap the HospitalView in a NavigationView
+                List() {
+                    ForEach(formatHospitals().keys.sorted(by: <), id: \.self) { key in
+                        Section(header: Text(key).foregroundColor(Color.black)) {
+                            VStack(alignment: .leading, spacing: 0){
+                                ForEach(formatHospitals()[key]!, id: \.self) { hospital in
+                                    NavigationLink(destination: ChatView()) { // Navigate to ChatView
+                                        HStack{
+                                            Image(hospital.image_url)
+                                                .resizable()
+                                                .frame(width: 30, height: 20)
+                                            Text(hospital.hospital_name)
+                                                .foregroundColor(Color.black)
+                                            Spacer()
+                                            Image(systemName: "arrow.right")
+                                                .foregroundColor(Color.black)
+                                        }//: HSTACK
+                                        .padding(20)
+                                    }
+                                    .buttonStyle(PlainButtonStyle()) // Remove button styling
+                                    .frame(maxWidth: .infinity, alignment: .leading) // Fill the entire row
+                                    .background(Color.clear) // Clear background
+                                }//: LOOP
+                            }//: VSTACK
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.black, lineWidth: 2)
+                            )
+                            .foregroundColor(Color(uiColor: .tertiaryLabel))
+                        }//: SECTION
+                        .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
+                    }//: LOOP
+            }//: LIST
+            .listStyle(InsetListStyle())
+        }
     }
-}
-
-
-struct HospitalView_Previews: PreviewProvider {
-    static var previews: some View {
-        HospitalView()
+    
+    
+    struct HospitalView_Previews: PreviewProvider {
+        static var previews: some View {
+            HospitalView()
+        }
     }
 }

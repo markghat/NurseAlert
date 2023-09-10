8b5936b977486d0bb7a8944a5dee1b98b46a873b
Ana Lucia Ochoa <analuciaochoa@MacBook-Air-3.local>
2023-09-10 00:32:28 -0700

    hospitals page

--- NurseAlert/NurseAlert/Hospital .swift
+++ NurseAlert/NurseAlert/Hospital .swift
@@ -15,9 +15,16 @@ struct Hospital: Identifiable, Hashable{
     
 }
 
-func getHospital() -> [Hospital]{
-    return [Hospital(id: 1, hospital_name: "Duke University Hospital", image_url: "Duke-Hospital-Logo"), Hospital(id: 2, hospital_name: "UNC", image_url: "Duke-Hospital-Logo")]
+func getHospital() -> [Hospital] {
+    return [Hospital(id: 1, hospital_name: "Duke University Hospital", image_url: "Duke-Hospital-Logo"), Hospital(id: 2, hospital_name: "UNC", image_url: "UNC-Hospital-Logo"), Hospital(id: 3, hospital_name: "Wake Forest", image_url: "WakeForest-Hospital-Logo"), Hospital(id: 4, hospital_name: "NC State", image_url: "NCState-Hospital-Logo")]
 
+}
+
+func formatHospitals() -> [String: [Hospital]]{
+    return ["Other Hospitals": [Hospital(id: 1, hospital_name: "Duke University Hospital", image_url: "Duke-Hospital-Logo"),
+                                Hospital(id: 2, hospital_name: "UNC", image_url: "UNC-Hospital-Logo")],
+            "Hospitals Near You": [Hospital(id: 3, hospital_name: "Wake Forest", image_url: "WakeForest-Hospital-Logo")]]
+    
 }
 
 //func callAPI() {

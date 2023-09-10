e9a8504b0bde81c6d535824f131835787c2f03ac
Ana Lucia Ochoa <analuciaochoa@MacBook-Air-3.local>
2023-09-10 01:56:03 -0700

    fixed button issue

--- NurseAlert/NurseAlert/HospitalView.swift
+++ NurseAlert/NurseAlert/HospitalView.swift
@@ -21,44 +21,33 @@ import SwiftUI
     //}
 //}
 
-struct HospitalView: View{
+struct HospitalView: View {
     
-    var titles = ["Other Hospitals" : ["Cedars Sinai", "Rush Hospital", "Mayo Clinic", "Johns Hopkins", "UCLA"], "Hospitals Near You" : ["Duke University", "UNC", "Wake Forest", "NC State"]]
-    
-    
+    var hospitals = getHospital() // Assuming you have a function to retrieve hospitals
     
     var body: some View {
-            NavigationView { // Wrap the HospitalView in a NavigationView
-                List() {
-                    ForEach(formatHospitals().keys.sorted(by: <), id: \.self) { key in
-                        Section(header: Text(key).foregroundColor(Color.black)) {
-                            VStack(alignment: .leading, spacing: 0){
-                                ForEach(formatHospitals()[key]!, id: \.self) { hospital in
-                                    NavigationLink(destination: ChatView()) { // Navigate to ChatView
-                                        HStack{
-                                            Image(hospital.image_url)
-                                                .resizable()
-                                                .frame(width: 30, height: 20)
-                                            Text(hospital.hospital_name)
-                                                .foregroundColor(Color.black)
-                                            Spacer()
-                                            Image(systemName: "arrow.right")
-                                                .foregroundColor(Color.black)
-                                        }//: HSTACK
-                                        .padding(20)
-                                    }
-                                    .buttonStyle(PlainButtonStyle()) // Remove button styling
-                                    .frame(maxWidth: .infinity, alignment: .leading) // Fill the entire row
-                                    .background(Color.clear) // Clear background
-                                }//: LOOP
-                            }//: VSTACK
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.black, lineWidth: 2)
-                            )
-                            .foregroundColor(Color(uiColor: .tertiaryLabel))
-                        }//: SECTION
-                        .listRowInsets(EdgeInsets(top: 0, leading: 16, bottom: 0, trailing: 16))
-                    }//: LOOP
+        NavigationView {
+            List(hospitals, id: \.self) { hospital in
+                NavigationLink(destination: ChatView()) { // Navigate to ChatView
+                    HStack{
+                        Image(hospital.image_url)
+                            .resizable()
+                            .frame(width: 30, height: 20)
+                        Text(hospital.hospital_name)
+                            .foregroundColor(Color.black)
+                        Spacer()
+                        Image(systemName: "arrow.right")
+                            .foregroundColor(Color.black)
+                    }//: HSTACK
+                    .padding(20)
+                }
+                .buttonStyle(PlainButtonStyle()) // Remove button styling
+                .frame(maxWidth: .infinity, alignment: .leading) // Fill the entire row
+                .background(Color.clear) // Clear background
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.black, lineWidth: 2)
+                )
+                .foregroundColor(Color(uiColor: .tertiaryLabel))
             }//: LIST
             .listStyle(InsetListStyle())
         }

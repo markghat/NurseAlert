e09efd2722b3483528db2a0dcdaf8fa38a600f2e
Mark Ghattas <mark.ghattas@duke.edu>
2023-09-09 23:35:28 -0400

    basic welcome screen

--- NurseAlert/NurseAlert/HomeView.swift
+++ NurseAlert/NurseAlert/HomeView.swift
@@ -1,15 +1,39 @@
-//
-//  HomeView.swift
-//  NurseAlert
-//
-//  Created by Tomas Esber on 9/9/23.
-//
-
 import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        ScrollView {
+        NavigationView {
+            VStack(spacing: 20) {
+                //MARK: - HEADER
+                Spacer()
+                
+                Image("Duke-Hospital-Logo")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                
+                //MARK: - CENTER
+                Text("Welcome")
+                    .font(.title3)
+                    .fontWeight(.light)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                
+                //MARK: - FOOTER
+                Spacer()
+
+                NavigationLink(destination: HospitalView()) {
+                    Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                        .imageScale(.large)
+                    Text("Contact Nurse")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.capsule)
+                .controlSize(.large)
+            }
         }
     }
 }

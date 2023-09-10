d48a992f673a1a8591b7db307f22ea26d87bfcdb
Ana Lucia Ochoa <analuciaochoa@MacBook-Air-3.local>
2023-09-10 04:56:44 -0700

    completed pop-up

--- NurseAlert/NurseAlert/HospitalView.swift
+++ NurseAlert/NurseAlert/HospitalView.swift
@@ -1,26 +1,18 @@
 import SwiftUI
 
-//struct HospitalView: View {
-    //var body: some View {
-        //List(getHospital(), id: \.self) { hospital in
-            //HStack {
-                //Image(hospital.image_url)
-                    //.resizable()
-                    //.frame(width: 30, height: 20)
-                //Text(hospital.hospital_name).fontWeight(.semibold)
-            //}
-           // .padding(.vertical, 20)
-       // }
-    //}
-//}
-
 struct HospitalView: View {
-    
+    @State private var selectedHospital: Hospital?
+    @State private var isPopUpVisible = false
+
     var hospitals = getHospital() // Assuming you have a function to retrieve hospitals
     
     var body: some View {
-            List(hospitals, id: \.self) { hospital in
-                NavigationLink(destination: RoomListView()) { // Navigate to ChatView
+        List {
+            ForEach(hospitals, id: \.self) { hospital in
+                Button(action: {
+                    selectedHospital = hospital
+                    isPopUpVisible.toggle()
+                }) {
                     HStack{
                         Image(hospital.image_url)
                             .resizable()
@@ -30,19 +22,77 @@ struct HospitalView: View {
                         Spacer()
                         Image(systemName: "arrow.right")
                             .foregroundColor(Color.black)
-                    }//: HSTACK
+                    }
                     .padding(20)
-                    .navigationTitle("Select Hospital")
-                    .fontDesign(.rounded)
                 }
-                .buttonStyle(PlainButtonStyle()) // Remove button styling
-                .frame(maxWidth: .infinity, alignment: .leading) // Fill the entire row
-                .background(Color.clear) // Clear background
+                .buttonStyle(PlainButtonStyle())
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .background(Color.clear)
                 .overlay(
                     RoundedRectangle(cornerRadius: 10, style: .circular).stroke(Color.black, lineWidth: 2)
                 )
                 .foregroundColor(Color(uiColor: .tertiaryLabel))
-            }//: LIST
-            .listStyle(InsetListStyle())
+            }
+        }
+        .listStyle(InsetListStyle())
+        .navigationTitle("Select Hospital")
+        .fontDesign(.rounded)
+        .overlay(
+            PopUpView(isVisible: $isPopUpVisible) {
+                VStack {
+                    Button(action: {
+                        // Action for the "Patient" button (e.g., connect to RoomListView)
+                        isPopUpVisible.toggle()
+                        // Add your navigation code here
+                    }) {
+                        Text("Patient")
+                            .padding()
+                    }
+                    Button(action: {
+                        // Action for the "Provider" button (e.g., do something else)
+                        isPopUpVisible.toggle()
+                    }) {
+                        Text("Provider")
+                            .padding()
+                    }
+                }
+            }
+        )
+    }
+}
+
+struct PopUpView<Content: View>: View {
+    @Binding var isVisible: Bool
+    let content: () -> Content
+    
+    var body: some View {
+        GeometryReader { geometry in
+            if isVisible {
+                ZStack {
+                    Color.black.opacity(0.4)
+                        .onTapGesture {
+                            isVisible = false
+                        }
+                    
+                    VStack {
+                        content()
+                            .background(Color.white)
+                            .cornerRadius(10)
+                            .padding()
+                    }
+                    .frame(width: geometry.size.width * 0.8, height: 150) // Adjust the size as needed
+                    .background(Color.clear)
+                    .cornerRadius(10)
+                }
+                .transition(.opacity)
+            }
+        }
+    }
+}
+
+
+struct HospitalView_Previews: PreviewProvider {
+    static var previews: some View {
+        HospitalView()
     }
 }
--- NurseAlert/NurseAlert/RoleSelectView.swift
+++ NurseAlert/NurseAlert/RoleSelectView.swift
@@ -9,12 +9,58 @@ import SwiftUI
 
 struct RoleSelectView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //NavigationLink(destination: RoomView()){
+        VStack {
+            Spacer()
+            Button(action: {
+                // Action for the first button
+            }) {
+                Text("Patient")
+                    .fontDesign(.rounded)
+                    .font(.system(size: 60))
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .frame(maxHeight: .infinity)
+                    .background(Color(UIColor(hex: "#007AFF")))
+                    .foregroundColor(.white)
+            }
+            
+            Button(action: {
+                // Action for the second button
+            }) {
+                Text("Provider")
+                    .fontDesign(.rounded)
+                    .font(.system(size: 60))
+                    .bold()
+                    .frame(maxWidth: .infinity)
+                    .frame(maxHeight: .infinity)
+                    .background(Color(UIColor(hex: "#22168c")))
+                    .foregroundColor(.white)
+            }
+            Spacer()
+        }
     }
 }
 
-struct RoleSelectView_Previews: PreviewProvider {
-    static var previews: some View {
-        RoleSelectView()
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
+        
+        struct RoleSelectView_Previews: PreviewProvider {
+            static var previews: some View {
+                RoleSelectView()
+            }
+        }
